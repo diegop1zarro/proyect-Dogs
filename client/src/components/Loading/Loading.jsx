945e8713e5e0b8e159da4721d531cc9ba6b279b9
diegop1zarro <diego.cab.1016@gmail.com>
2022-11-06 18:30:05 -0300

    estructura y css loading

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import '../../Style/Loading.css'
 export default function Loading (){
     return(
-        <div >
-            <div class="spinner"></div>
-           {/* <div className='Loadingmsj'>Loading...</div> */}
+        <div>
+           <div className="spinner"></div>
+           <p>Loading...</p>
         </div>
     )
 }
