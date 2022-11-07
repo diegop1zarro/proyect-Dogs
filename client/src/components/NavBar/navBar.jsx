250adc3941e76cd9c0ad7fbfef48df242d6a4a0b
diegop1zarro <diego.cab.1016@gmail.com>
2022-11-06 21:38:14 -0300

    css colors navbar

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../../Style/NavBar.css';
-import Logo from '../../logoHenry.png'
-import Dogimg from '../../dog.png'
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../Style/NavBar.css";
+import Logo from "../../logoHenry.png";
+import Dogimg from "../../dog.png";
 
 export default function NavBar() {
-    return (
-        <header className="navbar">
-            <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-                <img id="logoHenry" src={Dogimg} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </div>
-            <NavLink className='h1' to='/home'>
-            <h1 className='h1'> DOGS</h1>
+  return (
+    <header className="navbar">
+      <div>
+        <img
+          id="logoHenry"
+          src={Logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+        <img
+          id="logoHenry"
+          src={Dogimg}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+      </div>
+      <NavLink className="h1" to="/home">
+        <h1> DOGS</h1>
+      </NavLink>
+
+      <nav>
+        <ul className="list">
+          <li className="list-item">
+            <NavLink className="list-item" to="/home">
+              Home
             </NavLink>
-              
-            <nav>
-                <ul className="list">
-                    <li className="list-item">
-                        <NavLink className="list-item" to="/home" >Home</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-            
-    )
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
